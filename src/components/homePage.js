ac9f3e2ffa2ffd10074762b01b9faa25ac763452
Juan Romero Ramos <jromero@encamina.com>
2018-01-23 16:37:09 +0100

    change homePage.js

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -5,7 +5,7 @@ import { Jumbotron } from 'react-bootstrap';
 class Home extends React.Component {
     render() {
         return (
-          <Jumbotron>
+            <Jumbotron>
                 <h1>Pluralsight Administration</h1>
                 <p>React, React Router and Flux for ultra-responsive web apps.</p>
                 <Link to="/about" className="btn btn-primary btn-lg">Learn more</Link>
